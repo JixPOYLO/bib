02ccb3ea8ddb89d3969e6a44a862d74b38958f0d
JixPOYLO <jean-alix.poylo@devinci.fr>
2020-02-29 18:29:01 +0100

    update samedi 58 restaurants avec les deux labels

--- server/my-app/src/index.js
+++ server/my-app/src/index.js
@@ -6,7 +6,6 @@ import './App.css';
 import styles from './mystyle.module.css'; 
 
 ReactDOM.render(<p className={styles.bigblue}> the Project by JixPOYLO </p>, document.getElementById('sandy'));
-console.log("hey")
 ReactDOM.render(<App />, document.getElementById('root'));
 
-//
+console.log("jix");
