f6c98ef63076e375cc6009b876cc4f2c40adf45c
JixPOYLO <jean-alix.poylo@devinci.fr>
2020-03-03 15:10:48 +0100

    i copied 2 times restosBoth.json cause i ve one in my server file that i copied into my-app/src

--- server/my-app/src/index.js
+++ server/my-app/src/index.js
@@ -7,5 +7,5 @@ import styles from './components/mystyle.module.css';
 
 ReactDOM.render(<h1 className={styles.bigblue}> Project by JixPOYLO </h1>, document.getElementById('sandy'));
 
-console.log("jix")
+  
 ReactDOM.render(<App />, document.getElementById('root'));
